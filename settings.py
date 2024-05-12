ea10397be70c623007cf6ea765400c68e8d7048f
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-12 19:36:34 +0530

    settings update

--- settings.py
+++ settings.py
@@ -1,1 +1,2 @@
 ESRGAN_SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+prompt = "A blonde cat with white stripes on body sitting on a pink bicycle with black sunglasses in a desert on a rainy day"
